6a497ce698be1a0ff847b1a6aa7f7bc208d422c2
cheonyeji <ssaltuck@gmail.com>
2022-07-21 15:36:54 +0900

    Add State

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const enteredYearHandler = (enteredYear) => {
+    setFilteredYear(enteredYear);
+  };
   return (
-    <Card className="expenses">
-      {props.data.map((data) => (
-        <ExpenseItem title={data.title} amount={data.amount} date={data.date} />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onEnteredYear={enteredYearHandler}
+        />
+        {props.data.map((data) => (
+          <ExpenseItem
+            title={data.title}
+            amount={data.amount}
+            date={data.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
